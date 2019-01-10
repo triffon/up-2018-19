363f01c693cb3c890ef82355c2a3e5f8cf9e6d9b
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2019-01-10 10:57:46 +0200

    Рекурсивни функции за масиви

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -109,7 +109,140 @@ void testexpression() {
   
 }
 
+
+void printArray(int a[], int n) {
+  for(int i = 0; i < n; i++)
+    cout << a[i] << ' ';
+  cout << endl;
+}
+
+int readArray(int a[]) {
+  int n;
+  cout << "n = ";cin >> n;
+  for(int i = 0; i < n; i++) {
+    cout << "a[" << i << "] = ";
+    cin >> a[i];
+  }
+  return n;
+}
+
+const int MAX = 100;
+
+int sumArray(int a[], int n) {
+  if (n == 0)
+    return 0;
+  // return sumArray(a, n - 1) + a[n - 1];
+  return a[0] + sumArray(a + 1, n - 1);
+}
+
+bool search(int a[], int n, int x) {
+  return n > 0 && (a[0] == x || search(a + 1, n - 1, x));
+  
+  if (a[0] == x)
+    return true;
+  if (n == 0)
+    return false;
+  // a[0] != x
+  // n != 0
+  // if (p) return true; else return false; <-> return p;
+  /*
+  if (search(a + 1, n - 1, x))
+    return true;
+  else
+    return false;
+  */
+  return search(a + 1, n - 1, x);
+}
+
+bool sorted(int a[], int n) {
+  return n <= 1 || (a[0] <= a[1] && sorted(a + 1, n - 1));
+    
+  if (n == 1)
+    return true;
+  if (a[0] > a[1])
+    return false;
+  // n > 1
+  // a[0] <= a[1]
+  return sorted(a + 1, n - 1);
+}
+
+bool different(int a[], int n) {
+  // !!!  return n <= 1 || (a[0] != a[1] && different(a + 1, n - 1));
+  return n <= 1 || (!search(a + 1, n - 1, a[0]) && different(a + 1, n - 1));
+}
+
+// връща индекса на последния елемент в първата част на масива
+int split(int a[], int n, int pivot) {
+  int i = 0, j = n - 1;
+  while (i != j)
+    if (a[i] < pivot)
+      // a[i] е в правилната част на масива
+      i++;
+    else
+      // a[i] НЕ е в правилната част на масива
+      if (a[j] >= pivot)
+        // a[j] е в правилната част на масива
+        j--;
+      else
+        // a[j] НЕ е в правилната част на масива
+        // разменяме ги
+        swap(a[i], a[j]);
+  cout << "След разделянето масивът изглежда така: ";
+  printArray(a, n);
+  // i == j
+  // не знаем a[i] в коя част на масива е
+  if (a[i] < pivot)
+    // a[i] е в първата част на масива
+    return i;
+  // a[i] е във втората част на масива
+  return i - 1;
+}
+
+void quicksort(int a[], int n) {
+  if (n <= 1)
+    return;
+  cout << "Започваме да сортираме следния масив: ";
+  printArray(a, n);
+  // n > 1
+  // 1. избираме a[0] за ос
+  int& pivot = a[0];
+  cout << "Оста ни е: " << pivot << endl;
+  // 2. разделяме масива на две части спрямо оста
+  // lastless е индексът на последния елемент от първата част
+  int lastless = split(a + 1, n - 1, pivot) + 1;
+  cout << "Индексът на последния елемент от първата част е: " << lastless << endl;
+  // 3. поставяме оста между двете части на масива
+  // т.е. разменяме я с последния елемент от първата част
+  swap(pivot, a[lastless]);
+  // 4. сортираме рекурсивно двете части на масива без оста
+  quicksort(a, lastless); // + 1 - 1
+  quicksort(a + lastless + 1, n - 1 - lastless);
+  cout << "Сортирахме масива: ";
+  printArray(a, n);
+}
+
+void testarrays() {
+  int a[MAX];
+  int n = readArray(a);
+  cout << "Сумата от елементите е " << sumArray(a, n) << endl;
+  cout << "Числото 2 ";
+  if (!search(a, n, 2))
+    cout << "НЕ ";
+  cout << "се среща в масива" << endl;
+  cout << "Масивът ";
+  if (!sorted(a, n))
+    cout << "НЕ ";
+  cout << "е сортиран" << endl;
+  cout << "Масивът ";
+  if (!different(a, n))
+    cout << "НЕ ";
+  cout << "се състои от различни елементи" << endl;
+  quicksort(a, n);
+  printArray(a, n);
+}
+
 int main() {
   // testrecursion();
-  testexpression();
+  // testexpression();
+  testarrays();
 }
